c85bd7f00b538b648829f28746901ba8b632ae6d
Zain Gulbaz <zaingulbaz8@gmail.com>
2023-11-08 10:57:14 +0500

    watch files command

--- FileSystems/watch/watch.js
+++ FileSystems/watch/watch.js
@@ -2,12 +2,25 @@ const fs = require("fs/promises");
 
 
 (async()=>{
-  
-  const files= await fs.watch("./command.txt");
 
-  for (const file of files )
+  const fileHandler= await fs.open("./command.txt","r");   
+  
+  const files= fs.watch("./command.txt");
+
+  for await (const file of files )
   {
-    console.log(file);
+    if(file.eventType=="change");
+    {
+        const stats=await fileHandler.stat();
+        const size=stats["size"];
+        const buffer= Buffer.alloc(size);
+        const position=0;
+        const offset=0;
+
+        const fileBuffer= await fileHandler.read(buffer,offset,size,position);
+        console.log(fileBuffer["buffer"].toString("utf-8"));
+
+    }
   }
 
 
